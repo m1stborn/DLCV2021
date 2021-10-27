1c41bac0f9714ad24d3ee519f404e65a02566b43
m1stborn <kevin1997324@gmail.com>
2021-10-27 22:20:46 +0800

    feat: tSNE for report

--- HW1/model_p1/pretrained_resnet.py
+++ HW1/model_p1/pretrained_resnet.py
@@ -22,3 +22,34 @@ class PretrainedResnet(nn.Module):
         x = x.view(x.size(0), 2048)
         out = self.classifier(x)
         return out
+
+
+class ExtractedResnet(PretrainedResnet):
+    def __init__(self, num_classes=50):
+        super().__init__(num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 2048)
+        return x
+
+
+# if __name__ == '__main__':
+    # net = PretrainedResnet()
+    # net.to("cuda")
+    # print(net)
+
+    # net = ExtractedResnet()
+    # net.to("cuda")
+    # import torch
+    # inputs = torch.randn(3, 3, 224, 224).to(device='cuda')
+    # out = net(inputs)
+    # print(out.size())
+
+    # x = torch.randn(3, 2048)
+    # lst = [x for i in range(3)]
+    # print(len(lst))
+    # print(torch.cat(lst).size())
+    # val_pred = torch.cat(lst).cpu().numpy()
+    # print(val_pred.shape)
+
