70a2348dc73442fd58323f630399af1022f85ee2
m1stborn <r10922178@ntu.edu.tw>
2021-12-30 15:01:08 +0800

    Feat: modify test_testcase

---
+++ HW4/run_exp_p1.sh
@@ -0,0 +1,10 @@
+#python train_p1_3.py --exp1 --k 5
+#python train_p1_3.py --exp1 --k 10
+#python train_p1_3.py --exp1 --k 1
+
+python train_p1_2.py
+python train_p1_2.py --exp2 cosine
+#python train_p1_2.py --exp2 parametric
+
+#python test_p1.py --ckpt .\ckpt\p1\ProtoNet-euclidean-f34e28d9.pt  --output_csv .\result\p1\euclidean.csv
+#python test_p1.py --ckpt .\ckpt\p1\ProtoNet-euclidean-f34e28d9.pt  --output_csv .\result\p1\euclidean.csv
